14bdf11a2aaf1c36a0088194392198f54c65747e
SamueleBianchi <samuelebianchi1999@gmail.com>
2018-12-17 11:35:11 +0100

    Randomizzazione dei pericoli in ogni cella della mappa e dei oggetti

--- gamelib.c
+++ gamelib.c
@@ -7,6 +7,8 @@ static void crea_scacchiera();
 static void stampa_scacchiera();
 static void termina_creazione();
 int verifica();
+void randomizza_pericoli(int n,struct Cella *scacchiera);
+void randomizza_oggetti(int n,struct Cella *scacchiera);
 
 
 struct Cella *scacchiera = NULL;
@@ -52,6 +54,12 @@ static void crea_scacchiera(){
   if(scacchiera == NULL){
     printf("Impossibile creare la scacchiera.\n");
   }else{
+    randomizza_pericoli(n,scacchiera);
+    randomizza_oggetti(n,scacchiera);
+  }
+}
+
+void randomizza_pericoli(int n,struct Cella *scacchiera){
   int prob[3];
   int somma=0;
 
@@ -72,7 +80,6 @@ static void crea_scacchiera(){
     }else{
       break;
     }
-
   }
   time_t t;
   srand((unsigned) time(&t));
@@ -94,9 +101,65 @@ static void crea_scacchiera(){
 
 
     }
-  }
 }
 
+void randomizza_oggetti(int n,struct Cella *scacchiera){
+  int prob[5];
+  int somma=0;
+
+  while(somma<=100){
+    printf("Inserisci la probabilità che non ci sia alcun oggetto: ");
+    prob[0]= verifica();
+    somma+=prob[0];
+    printf("Inserisci la probabilità che ci sia un medikit: ");
+    prob[1]= verifica();
+    somma+=prob[1];
+    printf("Inserisci la probabilità ci sia una pozione: ");
+    prob[2]= verifica();
+    somma+=prob[2];
+    printf("Inserisci la probabilità ci sia un materiale: ");
+    prob[3]= verifica();
+    somma+=prob[3];
+    printf("Inserisci la probabilità ci siano colpi lanciarazzi: ");
+    prob[4]= verifica();
+    somma+=prob[4];
+    if(somma>100 || somma <100){
+      printf("Errore: la somma delle probabilità deve essere 100, riprovare\n");
+      somma=0;
+      continue;
+    }else{
+      break;
+    }
+  }
+  time_t t;
+  srand((unsigned) time(&t));
+  int random = 0;
+  for(int i=0; i<n;i++){
+    for(int j=0; j<n;j++){
+      random = rand() % 101;
+      if(random <= prob[0]){
+        scacchiera[i*n+j].oggetto = 0;
+      }else{
+        if(random <= prob[0]+prob[1]){
+          scacchiera[i*n+j].oggetto = 1;
+        }else{
+          if(random <= prob[0]+prob[1]+prob[2]){
+            scacchiera[i*n+j].oggetto = 2;
+          }else{
+            if(random <= prob[0]+prob[1]+prob[2]+prob[3]){
+              scacchiera[i*n+j].oggetto = 3;
+            }else{
+              scacchiera[i*n+j].oggetto = 4;
+            }
+          }
+        }
+      }
+        printf("random: %d pericolo %d:\n",random,scacchiera[i*n+j].oggetto);
+      }
+
+
+    }
+}
 
 static void stampa_scacchiera(){
 
@@ -111,7 +174,8 @@ void gioca(){
 }
 
 void termina_gioco(){
-
+  free(scacchiera);
+  scacchiera = NULL;
 }
 
 int verifica(){
